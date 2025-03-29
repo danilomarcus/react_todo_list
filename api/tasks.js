6131e760b8455e5f815f2c4d20b5c142caae3b5b
Danilo Marcus <danpayne16@msn.com>
2025-03-29 00:24:47 -0300

    teste

--- api/tasks.js
+++ api/tasks.js
@@ -1,7 +1,7 @@
 // api/tasks.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json'); // Caminho para o arquivo db.json
+const router = jsonServer.router({ tasks: [] }); // Caminho para o arquivo db.json
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
